b227918a55fb43a55b5a7e547bb1a5e39481435a
Prateek <prateek.verma0701@gmail.com>
2019-11-11 00:59:01 +0530

    Changes done in EmployeeBasicDetailResponseDTO and added Util class.

--- src/main/java/com/prateek/learning/employees/dto/EmployeeBasicDetailResponseDTO.java
+++ src/main/java/com/prateek/learning/employees/dto/EmployeeBasicDetailResponseDTO.java
@@ -1,47 +1,48 @@
 package com.prateek.learning.employees.dto;
 
+import com.prateek.learning.employees.entity.Address;
 import com.prateek.learning.employees.entity.Employee;
+import com.prateek.learning.employees.entity.Project;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.domain.Page;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.prateek.learning.employees.utils.EmployeeRecordUtils.COMMA_DELIMITER_WITH_SPACE;
+import static com.prateek.learning.employees.utils.EmployeeRecordUtils.SPACE;
 
 @Getter
 @Setter
 public class EmployeeBasicDetailResponseDTO {
+    private String fullName;
     private Long employeeContractorId;
     private String companyName;
     private Long companyId;
     private String departmentName;
-    private String firstName;
-    private String lastName;
+    private String projects;
     private String phoneNumber;
     private String email;
-    private String houseNumber;
-    private String addressLine1;
-    private String city;
+    private String address;
     private String state;
     private String country;
-    private String pinCode;
 
     public static EmployeeBasicDetailResponseDTO createEmployeeBasicResponseModel(Employee employee) {
         EmployeeBasicDetailResponseDTO employeeBasicDetailResponseDTO = new EmployeeBasicDetailResponseDTO();
+
         employeeBasicDetailResponseDTO.setEmployeeContractorId(employee.getEmployeeContractorId());
         employeeBasicDetailResponseDTO.setDepartmentName(employee.getDepartment().getDepartmentName());
+        employeeBasicDetailResponseDTO.setProjects(getEmployeeProjects(employee.getProjects()));
         employeeBasicDetailResponseDTO.setCompanyName(employee.getCompany().getCompanyName());
         employeeBasicDetailResponseDTO.setCompanyId(employee.getCompany().getCompanyId());
-        employeeBasicDetailResponseDTO.setFirstName(employee.getFirstName());
-        employeeBasicDetailResponseDTO.setLastName(employee.getLastName());
+        employeeBasicDetailResponseDTO.setFullName(employee.getFirstName().concat(SPACE).concat(employee.getLastName()));
         employeeBasicDetailResponseDTO.setPhoneNumber(employee.getPhoneNumber());
         employeeBasicDetailResponseDTO.setEmail(employee.getEmail());
-        employeeBasicDetailResponseDTO.setHouseNumber(employee.getAddress().getHouseNumber());
-        employeeBasicDetailResponseDTO.setAddressLine1(employee.getAddress().getAddressLine1());
-        employeeBasicDetailResponseDTO.setCity(employee.getAddress().getCity());
+        employeeBasicDetailResponseDTO.setAddress(getEmployeeAddress(employee.getAddress()));
         employeeBasicDetailResponseDTO.setState(employee.getAddress().getState());
         employeeBasicDetailResponseDTO.setCountry(employee.getAddress().getCountry());
-        employeeBasicDetailResponseDTO.setPinCode(employee.getAddress().getPinCode());
 
         return employeeBasicDetailResponseDTO;
     }
@@ -50,24 +51,37 @@ public class EmployeeBasicDetailResponseDTO {
         List<EmployeeBasicDetailResponseDTO> employeeBasicDetailResponseDTOList = new ArrayList<>();
         employees.forEach(employee -> {
             EmployeeBasicDetailResponseDTO employeeBasicDetailResponseDTO = new EmployeeBasicDetailResponseDTO();
+
             employeeBasicDetailResponseDTO.setEmployeeContractorId(employee.getEmployeeContractorId());
             employeeBasicDetailResponseDTO.setDepartmentName(employee.getDepartment().getDepartmentName());
+            employeeBasicDetailResponseDTO.setProjects(getEmployeeProjects(employee.getProjects()));
             employeeBasicDetailResponseDTO.setCompanyName(employee.getCompany().getCompanyName());
             employeeBasicDetailResponseDTO.setCompanyId(employee.getCompany().getCompanyId());
-            employeeBasicDetailResponseDTO.setFirstName(employee.getFirstName());
-            employeeBasicDetailResponseDTO.setLastName(employee.getLastName());
+            employeeBasicDetailResponseDTO.setFullName(employee.getFirstName().concat(SPACE).concat(employee.getLastName()));
             employeeBasicDetailResponseDTO.setPhoneNumber(employee.getPhoneNumber());
             employeeBasicDetailResponseDTO.setEmail(employee.getEmail());
-            employeeBasicDetailResponseDTO.setHouseNumber(employee.getAddress().getHouseNumber());
-            employeeBasicDetailResponseDTO.setAddressLine1(employee.getAddress().getAddressLine1());
-            employeeBasicDetailResponseDTO.setCity(employee.getAddress().getCity());
+            employeeBasicDetailResponseDTO.setAddress(getEmployeeAddress(employee.getAddress()));
             employeeBasicDetailResponseDTO.setState(employee.getAddress().getState());
             employeeBasicDetailResponseDTO.setCountry(employee.getAddress().getCountry());
-            employeeBasicDetailResponseDTO.setPinCode(employee.getAddress().getPinCode());
 
             employeeBasicDetailResponseDTOList.add(employeeBasicDetailResponseDTO);
 
         });
         return employeeBasicDetailResponseDTOList;
+    }
+
+    private static String getEmployeeProjects(List<Project> projects) {
+        return projects
+                .stream()
+                .map(Project::getProjectName)
+                .collect(Collectors.joining(COMMA_DELIMITER_WITH_SPACE));
+    }
+
+    private static String getEmployeeAddress(Address address) {
+        return address.getHouseNumber().concat(SPACE)
+                .concat(address.getAddressLine1().concat(COMMA_DELIMITER_WITH_SPACE)
+                        .concat(address.getAddressLine2()).concat(COMMA_DELIMITER_WITH_SPACE)
+                        .concat(address.getCity()).concat(COMMA_DELIMITER_WITH_SPACE)
+                        .concat(address.getPinCode()));
     }
 }
---
+++ src/main/java/com/prateek/learning/employees/utils/EmployeeRecordUtils.java
@@ -0,0 +1,6 @@
+package com.prateek.learning.employees.utils;
+
+public class EmployeeRecordUtils {
+    public static final String SPACE = " ";
+    public static final String COMMA_DELIMITER_WITH_SPACE = ", ";
+}
